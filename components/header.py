3a7d27d7a14c0d92000e4e68c57155d2848137e9
Fariha2024 <164943878+Fariha2024@users.noreply.github.com>
2025-03-03 19:19:54 +0400

    Updated BMI Calculator chaned size

--- components/header.py
+++ components/header.py
@@ -4,10 +4,14 @@ def load_header():
     st.markdown(
         """
         <div style="background-color:#277e07; padding:10px; border-radius:10px; display: flex; align-items: center;">
-            <h1 style="color:white; margin-left: 0px; display: inline;">
+            <h1 style="color:white; margin-left: 0px;  font-size: 24px; display: inline;">
                <span style="color:#61fe28;">UnitConverters</span><span style="color:white;">.st</span>
             </h1>
         </div>
         """,
         unsafe_allow_html=True
     )
+
+
+
+  
